f7e52a6f7e188a418c68b7a28f0e36bc503c225f
Rhyhe <143746324+rhyhe@users.noreply.github.com>
2023-12-03 19:23:24 -0500

    removed comment and changed name of folder Item to item

--- src/main/java/rhyhe/rhyhetweaks/ModItems.java
+++ src/main/java/rhyhe/rhyhetweaks/ModItems.java
@@ -6,20 +6,19 @@ import net.minecraft.core.item.tool.*;
 import turniplabs.halplibe.helper.ItemHelper;
 
 public class ModItems {
-	// method is weird???
 	private static final ToolMaterial steeledGold = new ToolMaterial()
 		.setDurability(ToolMaterial.steel.getDurability() / 2)
-		.setEfficiency(ToolMaterial.iron.getEfficiency(false), ToolMaterial.steel.getEfficiency(true))
-		.setMiningLevel(ToolMaterial.steel.getMiningLevel())
-		.setSilkTouch(ToolMaterial.gold.isSilkTouch());
+			.setEfficiency(ToolMaterial.iron.getEfficiency(false), ToolMaterial.steel.getEfficiency(true))
+			.setMiningLevel(ToolMaterial.steel.getMiningLevel())
+			.setSilkTouch(ToolMaterial.gold.isSilkTouch());
 
-	public static Item toolPickaxeSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolPickaxe("tool.pickaxe.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenpickaxe", "tool_pickaxe_steeledgold.png");
-	public static Item toolAxeSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolAxe("tool.axe.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenaxe", "tool_axe_steeledgold.png");
-	public static Item toolShovelSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolShovel("tool.shovel.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenshovel", "tool_shovel_steeledgold.png");
-	public static Item toolSwordSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolSword("tool.sword.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldensword", "tool_sword_steeledgold.png");
-	public static Item toolHoeSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolHoe("tool.hoe.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenhoe", "tool_hoe_steeledgold.png");
+		public static Item toolPickaxeSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolPickaxe("tool.pickaxe.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenpickaxe", "tool_pickaxe_steeledgold.png");
+		public static Item toolAxeSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolAxe("tool.axe.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenaxe", "tool_axe_steeledgold.png");
+		public static Item toolShovelSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolShovel("tool.shovel.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenshovel", "tool_shovel_steeledgold.png");
+		public static Item toolSwordSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolSword("tool.sword.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldensword", "tool_sword_steeledgold.png");
+		public static Item toolHoeSteeledGolden = ItemHelper.createItem(RhyheTweaks.MOD_ID, new ItemToolHoe("tool.hoe.steeledgold", ItemHelper.findOpenIds(1), steeledGold), "steeledgoldenhoe", "tool_hoe_steeledgold.png");
 
-	public static void initialize(){
+		public static void initialize(){
 
 	}
 
